ade1ca9779f226a8dfafae24bdedaf09e80ff2a3
AJ ONeal <coolaj86@gmail.com>
2012-12-17 19:22:32 +0000

    added old deploy and a blank style.less

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ ! -f "less.min.js" ]
+then
+  wget "http://lesscss.googlecode.com/files/less-1.1.3.min.js" -O less.min.js
+fi
+
+if [ ! -f "javascripts/CFInstall.min.js" ]
+then
+  wget "http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js" -O javascripts/CFInstall.min.js
+fi
+
+if [ ! -f "javascripts/global-es5.js" ]
+then
+  wget "https://raw.github.com/kriskowal/narwhal-lib/3dba395ec5da18f1ef47aa651e10896764d83270/lib/narwhal/global-es5.js" -O javascripts/global-es5.js
+fi
+
+lessc style.less > style.css
+jade *.jade
+
+# hack for byId issues
+if [ ! -f "node_modules/qwery/qwery.rvagg.js" ]
+then
+  wget https://raw.github.com/rvagg/qwery/byId/src/qwery.js -O node_modules/qwery/qwery.rvagg.js
+  cp node_modules/qwery/qwery.rvagg.js node_modules/qwery/qwery.js
+fi
+pakmanager build
+cat pakmanaged.js > pakmanaged.tmp.js
+echo 'window.FormData = window.FormData || function FormData() {};' >> pakmanaged.js
+cat pakmanaged.tmp.js >> pakmanaged.js
+uglifyjs pakmanaged.js > pakmanaged.min.js
+#gzip pakmanaged.min.js -c > pakmanaged.min.js.gz
+rm -f pakmanaged.tmp.js pakmanaged.html
+sed -e "s/# VERSION.*/# VERSION `date +%s`/" main.appcache.tpl > main.appcache
